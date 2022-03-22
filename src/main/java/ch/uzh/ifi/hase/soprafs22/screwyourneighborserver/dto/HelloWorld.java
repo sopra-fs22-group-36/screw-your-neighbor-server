0bc532587c292e551ed2f8bbb500d266a5d2f7de
elBeato <beatus.furrer@gmail.com>
2022-03-22 23:16:49 +0100

    Update class diagram
    More precise definition of Cards, Deck, CardSource

--- src/main/java/ch/uzh/ifi/hase/soprafs22/screwyourneighborserver/dto/HelloWorld.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/screwyourneighborserver/dto/HelloWorld.java
@@ -1,11 +1,18 @@
 package ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.dto;
 
-public class HelloWorld {
+import java.util.Iterator;
+
+public class HelloWorld implements Iterable {
   @SuppressWarnings("FieldCanBeLocal")
   private final String hello = "Hello world";
 
   @SuppressWarnings("unused")
   public String getHello() {
     return hello;
+  }
+
+  @Override
+  public Iterator iterator() {
+    return null;
   }
 }
