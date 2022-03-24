79de52d5196a73f223a856efbf96a4431a58d2f9
BacLuc <lucius.bachmann@gmx.ch>
2022-03-24 17:27:17 +0100

    disable PUT on all endpoints, enable DELETE only on Player
    Issue: #5

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/screwyourneighborserver/entity/SpringRestConfiguration.java
@@ -0,0 +1,40 @@
+package ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.entity;
+
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.core.mapping.ConfigurableHttpMethods;
+import org.springframework.data.rest.core.mapping.ExposureConfiguration;
+import org.springframework.data.rest.core.mapping.ResourceMetadata;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class SpringRestConfiguration {
+  private static final List<Class<?>> ENTITIES_WITH_DELETE = List.of(Player.class);
+
+  @Bean
+  public RepositoryRestConfigurer repositoryRestConfigurer() {
+    return new RepositoryRestConfigurer() {
+      @Override
+      public void configureRepositoryRestConfiguration(
+          RepositoryRestConfiguration config, CorsRegistry cors) {
+        RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
+        ExposureConfiguration exposureConfiguration = config.getExposureConfiguration();
+        exposureConfiguration.withItemExposure(
+            (resourceMetaData, httpMethods) -> disableMethods(resourceMetaData, httpMethods));
+      }
+    };
+  }
+
+  private ConfigurableHttpMethods disableMethods(
+      ResourceMetadata resourceMetadata, ConfigurableHttpMethods httpMethods) {
+    httpMethods = httpMethods.disable(HttpMethod.PUT);
+    if (!ENTITIES_WITH_DELETE.contains(resourceMetadata.getDomainType())) {
+      httpMethods = httpMethods.disable(HttpMethod.DELETE);
+    }
+    return httpMethods;
+  }
+}
