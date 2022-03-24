64321e94fe276bd825eb99aef0a225a8da13b75d
Salome Wildermuth <salome.wildermuth@gmail.com>
2022-03-24 20:58:05 +0100

    Wieder ist der Kommentar reingeraten *_*

--- src/main/java/ch/uzh/ifi/hase/soprafs22/screwyourneighborserver/controller/HelloWorldController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/screwyourneighborserver/controller/HelloWorldController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-//Comment
 @RestController
 public class HelloWorldController {
   @GetMapping("/")
