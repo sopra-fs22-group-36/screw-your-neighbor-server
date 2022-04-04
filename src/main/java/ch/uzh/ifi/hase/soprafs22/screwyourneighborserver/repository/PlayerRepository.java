bad5c325c1d2ce4d2da3a4a8704ed958d99dbdd4
BacLuc <lucius.bachmann@gmx.ch>
2022-04-04 10:00:23 +0200

    security: replace validation class which made access control with spring SpEL
    https://docs.spring.io/spring-framework/docs/4.3.12.RELEASE/spring-framework-reference/html/expressions.html.
    Because the @PreAuthorize hook also is called
    when used in tests, we use the saveAll method
    which is not exposed.

--- src/main/java/ch/uzh/ifi/hase/soprafs22/screwyourneighborserver/repository/PlayerRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/screwyourneighborserver/repository/PlayerRepository.java
@@ -5,9 +5,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.parameters.P;
 
 @RepositoryRestResource
 public interface PlayerRepository extends PagingAndSortingRepository<Player, Long> {
   @SuppressWarnings("unused")
   Page<Player> findAllByName(String name, Pageable page);
+
+  @Override
+  @PreAuthorize(
+      "(hasRole('PLAYER') and #player.id == principal.id) or (!isAuthenticated() and #player.id == null)")
+  <S extends Player> S save(@P("player") S entity);
 }
