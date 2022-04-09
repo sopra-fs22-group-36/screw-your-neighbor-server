e825333a1c1e29c5718502759fd71d169014394f
MorisCamporesi <moris.camporesi@uzh.ch>
2022-04-09 21:50:40 +0200

    hand: add property announcedScore
    This is way simpler than adding the entity ScoreAnnouncement.
    We also had problems with infinite recursion with the ScoreAnnouncement entity.
    
    Issue: #62

--- src/main/java/ch/uzh/ifi/hase/soprafs22/screwyourneighborserver/repository/ScoreAnnouncementRepository.java
+++
@@ -1,9 +0,0 @@
-package ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.repository;
-
-import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.entity.ScoreAnnouncement;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface ScoreAnnouncementRepository
-    extends PagingAndSortingRepository<ScoreAnnouncement, Long> {}
