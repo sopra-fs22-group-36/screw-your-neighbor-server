601524c823009a67c600c8951f335047dab77773
BacLuc <lucius.bachmann@gmx.ch>
2022-04-09 17:54:31 +0200

    Card: add projections that the card is embedded
    Issue: #5

--- src/main/java/ch/uzh/ifi/hase/soprafs22/screwyourneighborserver/entity/CardEmbedProjection.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/screwyourneighborserver/entity/CardEmbedProjection.java
@@ -1,12 +1,16 @@
 package ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.entity;
 
-import java.util.Collection;
 import org.springframework.data.rest.core.config.Projection;
 
 @Projection(
     name = "embed",
-    types = {Hand.class})
-public interface HandEmbedProjection {
+    types = {Card.class})
+public interface CardEmbedProjection {
+  Hand getHand();
 
-  Collection<CardEmbedProjection> getCards();
+  Round getRound();
+
+  CardRank getCardRank();
+
+  CardSuit getCardSuit();
 }
