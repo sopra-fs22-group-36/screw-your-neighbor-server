e8f5071c947e3e054b9b9d0aab65a9bb60a59d98
Salome Wildermuth <salome.wildermuth@gmail.com>
2022-04-15 17:19:54 +0200

    added countByRound query

--- src/main/java/ch/uzh/ifi/hase/soprafs22/screwyourneighborserver/repository/CardRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/screwyourneighborserver/repository/CardRepository.java
@@ -2,8 +2,14 @@ package ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.repository;
 
 import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.entity.Card;
 import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.entity.CardEmbedProjection;
+import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.entity.Round;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(excerptProjection = CardEmbedProjection.class)
-public interface CardRepository extends JpaRepository<Card, Long> {}
+public interface CardRepository extends JpaRepository<Card, Long> {
+  @Query("SELECT COUNT(c) FROM Card c WHERE c.round=:round")
+  Long countByRound(@Param("round") Round round);
+}
