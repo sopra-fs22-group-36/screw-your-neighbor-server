ff28458c8dcc88157475eb0c9a67d40579e38d50
BacLuc <lucius.bachmann@gmx.ch>
2022-04-15 14:27:20 +0200

    Round: expose endpoint that we have links
    And also add roundNumber
    
    Issue: #43

--- src/main/java/ch/uzh/ifi/hase/soprafs22/screwyourneighborserver/repository/RoundRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/screwyourneighborserver/repository/RoundRepository.java
@@ -1,8 +1,9 @@
 package ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.repository;
 
 import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.entity.Round;
+import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.entity.RoundEmbedProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(exported = false)
+@RepositoryRestResource(excerptProjection = RoundEmbedProjection.class)
 public interface RoundRepository extends JpaRepository<Round, Long> {}
