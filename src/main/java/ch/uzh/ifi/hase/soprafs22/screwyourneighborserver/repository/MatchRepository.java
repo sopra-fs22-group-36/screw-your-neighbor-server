c74927536a4211c3efc9e0486d573067469095aa
BacLuc <lucius.bachmann@gmx.ch>
2022-04-15 14:58:18 +0200

    Expose MatchRepository
    And define projection for GameRepository.
    
    Issue: #43

--- src/main/java/ch/uzh/ifi/hase/soprafs22/screwyourneighborserver/repository/MatchRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/screwyourneighborserver/repository/MatchRepository.java
@@ -1,12 +1,13 @@
 package ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.repository;
 
 import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.entity.Match;
+import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.entity.MatchEmbedProjection;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(exported = false)
+@RepositoryRestResource(excerptProjection = MatchEmbedProjection.class)
 public interface MatchRepository extends JpaRepository<Match, Long> {
   List<Match> findAllByMatchNumber(@Param("matchNumber") int matchNumber);
 }
