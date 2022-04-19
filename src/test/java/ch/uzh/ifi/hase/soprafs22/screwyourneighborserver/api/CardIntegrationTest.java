451bc86d888a276bba4c8c6c3107ef0b4f131b64
Salome Wildermuth <salome.wildermuth@gmail.com>
2022-04-19 12:08:49 +0200

    test for new round creation

--- src/test/java/ch/uzh/ifi/hase/soprafs22/screwyourneighborserver/api/CardIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs22/screwyourneighborserver/api/CardIntegrationTest.java
@@ -6,52 +6,85 @@ import static org.mockito.Mockito.*;
 import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.entity.*;
 import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.repository.*;
 import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.sideeffects.CardEventHandler;
+import java.util.ArrayList;
+import java.util.Collection;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.mockito.Spy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CardIntegrationTest {
 
-  private RoundRepository roundRepo = mock(RoundRepository.class);
-  private CardRepository cardRepo = mock(CardRepository.class);
+  private RoundRepository roundRepoMock = mock(RoundRepository.class);
+  private CardRepository cardRepoMock = mock(CardRepository.class);
+  @Autowired private RoundRepository roundRepository;
+  @Autowired private MatchRepository matchRepository;
+  @Autowired private CardRepository cardRepository;
+  @Autowired private GameRepository gameRepository;
+  @Autowired private ParticipationRepository participationRepository;
 
-  @Spy private CardEventHandler cardEventHandler = new CardEventHandler(roundRepo, cardRepo);
+  @Spy
+  private CardEventHandler cardEventHandler = new CardEventHandler(roundRepoMock, cardRepoMock);
   // @Mock
   // private CardEventHandler cardEventHandler = new CardEventHandler(roundRepo, cardRepo);
   // private CardEventHandler cardEventHandler =
   //        Mockito.mock(CardEventHandler.class, withSettings().useConstructor(roundRepo,
   // cardRepo));
 
-  Match match = Mockito.mock(Match.class);
-  Round round = Mockito.mock(Round.class);
+  Match matchMock = Mockito.mock(Match.class);
+  Round roundMock = Mockito.mock(Round.class);
   CardRank cardRank = CardRank.ACE;
   CardSuit cardSuit = CardSuit.DIAMOND;
-  Card card_1 = Mockito.mock(Card.class, withSettings().useConstructor(cardRank, cardSuit));
+  Card cardMock = Mockito.mock(Card.class, withSettings().useConstructor(cardRank, cardSuit));
+
+  Participation participation_1 = new Participation();
+  Participation participation_2 = new Participation();
+  Collection<Participation> participations = new ArrayList<>();
+  Game game = new Game();
+  Match match = new Match();
+  Round round = new Round();
+  Card card = new Card();
 
   @BeforeEach
   @AfterEach
-  public void setup() {
-    Mockito.doReturn(round).when(card_1).getRound();
-    Mockito.doReturn(match).when(round).getMatch();
-  }
+  public void setup() {}
 
   @Test
   public void play_not_last_card_no_new_round() {
+    Mockito.doReturn(roundMock).when(cardMock).getRound();
+    Mockito.doReturn(matchMock).when(roundMock).getMatch();
     Mockito.doReturn(1).when(cardEventHandler).getNumberOfPlayedCards(any(Round.class));
     Mockito.doReturn(2).when(cardEventHandler).getNumberOfPlayers(any(Round.class));
-    cardEventHandler.handleAfterSave(card_1);
+    cardEventHandler.handleAfterSave(cardMock);
     verify(cardEventHandler, never()).createRound(any(Match.class), anyInt());
   }
 
   @Test
   public void play_last_card_new_round() {
+    Mockito.doReturn(roundMock).when(cardMock).getRound();
+    Mockito.doReturn(matchMock).when(roundMock).getMatch();
     Mockito.doReturn(2).when(cardEventHandler).getNumberOfPlayedCards(any(Round.class));
     Mockito.doReturn(2).when(cardEventHandler).getNumberOfPlayers(any(Round.class));
-    cardEventHandler.handleAfterSave(card_1);
+    cardEventHandler.handleAfterSave(cardMock);
     verify(cardEventHandler, times(1)).createRound(any(Match.class), anyInt());
+  }
+
+  @Test
+  public void create_new_round() {
+    matchRepository.save(match);
+    round.setMatch(match);
+    round.setRoundNumber(1);
+    roundRepository.save(round);
+    card.setRound(round);
+    cardRepository.save(card);
+    CardEventHandler cardEventHandler = new CardEventHandler(roundRepository, cardRepository);
+    Round newRound = cardEventHandler.createRound(match, round.getRoundNumber());
+    assertEquals(2, newRound.getRoundNumber());
+    Collection<Round> savedRounds = roundRepository.findAll();
+    assertTrue(savedRounds.stream().anyMatch(r -> r.getRoundNumber() == 2));
   }
 }
