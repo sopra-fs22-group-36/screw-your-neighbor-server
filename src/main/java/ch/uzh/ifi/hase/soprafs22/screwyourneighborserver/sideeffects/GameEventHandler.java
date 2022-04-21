dda0150b6b9b7868adb506888d15e927bf0dac45
Salome Wildermuth <salome.wildermuth@gmail.com>
2022-04-21 14:25:28 +0200

    change round and match sequence number value in signature

--- src/main/java/ch/uzh/ifi/hase/soprafs22/screwyourneighborserver/sideeffects/GameEventHandler.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/screwyourneighborserver/sideeffects/GameEventHandler.java
@@ -2,6 +2,7 @@ package ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.sideeffects;
 
 import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.entity.*;
 import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.repository.*;
+import java.util.List;
 import javax.transaction.Transactional;
 import org.springframework.data.rest.core.annotation.HandleAfterCreate;
 import org.springframework.data.rest.core.annotation.HandleAfterSave;
@@ -16,27 +17,17 @@ import org.springframework.web.client.HttpClientErrorException;
 @RepositoryEventHandler
 public class GameEventHandler {
   private final ParticipationRepository participationRepository;
-  private final MatchRepository matchRepo;
-  private final HandRepository handRepo;
-  private final CardRepository cardRepo;
-  private final RoundRepository roundRepo;
-  private final GameRepository gameRepo;
-  private CardDeck cardDeck;
+  private final GameRepository gameRepository;
+
   private final ModelFactory modelFactory;
 
   public GameEventHandler(
       ParticipationRepository participationRepository,
-      MatchRepository matchRepo,
-      HandRepository handRepo,
-      CardRepository cardRepo,
-      RoundRepository roundRepo,
-      GameRepository gameRepo) {
+      GameRepository gameRepository,
+      ModelFactory modelFactory) {
     this.participationRepository = participationRepository;
-    this.matchRepo = matchRepo;
-    this.handRepo = handRepo;
-    this.cardRepo = cardRepo;
-    this.roundRepo = roundRepo;
-    this.gameRepo = gameRepo;
+    this.gameRepository = gameRepository;
+    this.modelFactory = modelFactory;
   }
 
   @SuppressWarnings("unused")
@@ -61,28 +52,28 @@ public class GameEventHandler {
   @HandleAfterSave
   public void handleAfterSave(Game game) {
     if (game.getGameState().equals(GameState.PLAYING) && game.getMatches().isEmpty()) {
-      instanceCreator = new InstanceCreator(roundRepo, cardRepo, matchRepo, handRepo);
-      instanceCreator.assignParticipationNumbers(game);
+      modelFactory.assignParticipationNumbers(game);
 
       // Create first match with first round and save them
-      Match match = instanceCreator.createMatch(game);
-      Round round = instanceCreator.createRound(match, 0);
+      Match match = modelFactory.createMatch(game, 1);
+      Round round = modelFactory.addRound(match, 1);
 
       // Create a standard card deck (currently there's only this one)
-      cardDeck = new StandardCardDeck();
+      CardDeck cardDeck = new StandardCardDeck();
       // In the first round, 5 cards per player are distributed.
       int numOfCards = 5;
 
       // for each player that participates in the game we create a hand
       for (var participation : game.getParticipations()) {
-        Hand hand = instanceCreator.createHand(match, participation);
+        Hand hand = modelFactory.createHand(match, participation);
 
         for (int j = 0; j < numOfCards; j++) {
-          Card card = instanceCreator.createCard(hand, cardDeck);
+          modelFactory.addCardTo(hand, cardDeck.drawCard());
         }
       }
       // Because we configured CascadeType.ALL from game downwards, we only need one save.
-      gameRepo.save(game);
+      // gameRepository.save(game);
+      gameRepository.saveAll(List.of(game));
     }
   }
 }
