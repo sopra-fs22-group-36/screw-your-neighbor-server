fa1ce11f4f7f7d7cd327f2a80a9771e79b99de73
Salome Wildermuth <salome.wildermuth@gmail.com>
2022-04-21 14:22:59 +0200

    tests with GameBuilder, test for new match creation added

--- src/main/java/ch/uzh/ifi/hase/soprafs22/screwyourneighborserver/sideeffects/CardEventHandler.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/screwyourneighborserver/sideeffects/CardEventHandler.java
@@ -2,12 +2,11 @@ package ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.sideeffects;
 
 import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.entity.*;
 import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.repository.*;
+import java.util.List;
 import javax.transaction.Transactional;
 import org.springframework.data.rest.core.annotation.HandleAfterSave;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 @RepositoryEventHandler
@@ -15,10 +14,13 @@ public class CardEventHandler {
 
   private final ModelFactory modelFactory;
   private final GameRepository gameRepository;
+  private final RoundRepository roundRepository;
 
-  public CardEventHandler(ModelFactory modelFactory, GameRepository gameRepository) {
+  public CardEventHandler(
+      ModelFactory modelFactory, GameRepository gameRepository, RoundRepository roundRepository) {
     this.modelFactory = modelFactory;
     this.gameRepository = gameRepository;
+    this.roundRepository = roundRepository;
   }
 
   @SuppressWarnings("unused")
@@ -35,13 +37,12 @@ public class CardEventHandler {
     int numberOfPlayedCardsInRound = round.getCards().size();
     long numberOfHands = match.getHands().size();
     // last card in round was played
-    //noinspection ConstantConditions @BacLuc: i don't know why this is needed in my intellij
     if (numberOfPlayedCardsInRound >= numberOfHands) {
       int numberOfCardsPerPlayer = card.getHand().getCards().size();
       int numberOfPlayedRounds = match.getRounds().size();
       // no cards remaining in any hand
       if (numberOfPlayedRounds >= numberOfCardsPerPlayer) {
-        Match newMatch = modelFactory.createMatch(game);
+        Match newMatch = modelFactory.createMatch(game, match.getMatchNumber() + 1);
         CardDeck cardDeck = new StandardCardDeck();
         int numOfCards = numberOfCardsPerPlayer - 1;
         for (var participation : game.getParticipations()) {
@@ -49,12 +50,13 @@ public class CardEventHandler {
           for (int j = 0; j < numOfCards; j++) {
             modelFactory.addCardTo(hand, cardDeck.drawCard());
           }
-          modelFactory.addRound(newMatch, round.getRoundNumber());
         }
+        modelFactory.addRound(newMatch, round.getRoundNumber() + 1);
       } else {
-        modelFactory.addRound(match, round.getRoundNumber());
+        modelFactory.addRound(match, round.getRoundNumber() + 1);
       }
+      gameRepository.saveAll(List.of(game));
+      System.out.println("finished");
     }
-    gameRepository.saveAll(List.of(game));
   }
 }
