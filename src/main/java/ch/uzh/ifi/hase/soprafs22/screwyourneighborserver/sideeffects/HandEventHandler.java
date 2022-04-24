9654d3008ea3d0623409cc7c39ea50e5d0f49a4a
elBeato <beatus.furrer@gmail.com>
2022-04-24 17:18:15 +0200

    Check whether all players of the match announced there tricks

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/screwyourneighborserver/sideeffects/HandEventHandler.java
@@ -0,0 +1,68 @@
+package ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.sideeffects;
+
+import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.entity.Hand;
+import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.entity.Match;
+import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.entity.MatchState;
+import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.repository.HandRepository;
+import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.repository.MatchRepository;
+import org.springframework.data.rest.core.annotation.HandleAfterSave;
+import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Component
+@RepositoryEventHandler
+public class HandEventHandler {
+
+    private final MatchRepository matchRepository;
+    private final HandRepository handRepository;
+
+    public HandEventHandler(
+            MatchRepository matchRepository,
+            HandRepository handRepository) {
+        this.matchRepository = matchRepository;
+        this.handRepository = handRepository;
+    }
+
+    @SuppressWarnings("unused")
+    @HandleAfterSave
+    public void onAfterSave(Hand hand) {
+        long idMatch = hand.getMatch().getId();
+        Match myMatch = matchRepository.findById(idMatch).orElseThrow(() -> new EntityNotFoundException(idMatch + ""));
+        Collection<Hand> allHands = getAllHandsFromMatch(hand);
+        if (allPlayersAnnouncedScore(allHands)) {
+            myMatch.setMatchState(MatchState.PLAYING);
+            matchRepository.save(myMatch);
+        }
+    }
+
+    /**
+     * Checks whether all players from the match did the announcing
+     * @param hands
+     * @return
+     */
+    private boolean allPlayersAnnouncedScore(Collection<Hand> hands){
+        for (Hand el : hands) {
+            if (el.getAnnouncedScore() == null)
+                return false;
+        }
+        return true;
+    }
+
+    /**
+     * Find all Hands in the repository which belongs to this match
+     * @param hand announced score from a user hand in a game
+     * @return List from all hands
+     */
+    private Collection<Hand> getAllHandsFromMatch(Hand hand){
+        Collection<Hand> allHands = new ArrayList();
+        for (Hand el : handRepository.findAll()) {
+            if (el.getMatch() == hand.getMatch())
+                allHands.add(el);
+        }
+        return allHands;
+    }
+}
