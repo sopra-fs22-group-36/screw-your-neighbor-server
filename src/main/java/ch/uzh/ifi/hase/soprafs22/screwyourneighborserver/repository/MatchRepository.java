9ed1278e7d1c376707a08f61723174032eb0f104
elBeato <beatus.furrer@gmail.com>
2022-04-24 17:18:15 +0200

    Check whether all players of the match announced there tricks

--- src/main/java/ch/uzh/ifi/hase/soprafs22/screwyourneighborserver/repository/MatchRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/screwyourneighborserver/repository/MatchRepository.java
@@ -2,12 +2,12 @@ package ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.repository;
 
 import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.entity.Match;
 import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.entity.MatchEmbedProjection;
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
+import java.util.Optional;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(excerptProjection = MatchEmbedProjection.class)
-public interface MatchRepository extends JpaRepository<Match, Long> {
-  List<Match> findAllByMatchNumber(@Param("matchNumber") int matchNumber);
+public interface MatchRepository extends PagingAndSortingRepository<Match, Long> {
+    Optional<Match> findById(long id);
 }
