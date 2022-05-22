3cd2f2979c9f72effd6216c39776736dcd274685
BacLuc <lucius.bachmann@gmx.ch>
2022-05-22 17:14:36 +0200

    repositories: remove unused methods

--- src/main/java/ch/uzh/ifi/hase/soprafs22/screwyourneighborserver/repository/CardRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/screwyourneighborserver/repository/CardRepository.java
@@ -2,12 +2,8 @@ package ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.repository;
 
 import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.entity.Card;
 import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.entity.CardEmbedProjection;
-import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.entity.Round;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.parameters.P;
 
@@ -17,8 +13,4 @@ public interface CardRepository extends JpaRepository<Card, Long> {
   @SuppressWarnings({"SpringElInspection", "ELValidationInspection"})
   @PreAuthorize("hasRole('PLAYER') and isOwnCard(#card)")
   <S extends Card> S save(@P("card") S entity);
-
-  @RestResource(exported = false)
-  @Query("SELECT COUNT(c) FROM Card c WHERE c.round=:round")
-  Long countByRound(@Param("round") Round round);
 }
