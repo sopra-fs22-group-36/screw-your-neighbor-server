5df2e2578503ddfc0b2db0987279a97ca5be54c6
BacLuc <lucius.bachmann@gmx.ch>
2022-05-22 17:21:20 +0200

    RoundRepository: don't expose PATCH and POST
    Issue: #185

--- src/main/java/ch/uzh/ifi/hase/soprafs22/screwyourneighborserver/repository/RoundRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/screwyourneighborserver/repository/RoundRepository.java
@@ -4,6 +4,13 @@ import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.entity.Round;
 import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.entity.RoundEmbedProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.lang.NonNull;
 
 @RepositoryRestResource(excerptProjection = RoundEmbedProjection.class)
-public interface RoundRepository extends JpaRepository<Round, Long> {}
+public interface RoundRepository extends JpaRepository<Round, Long> {
+  @Override
+  @NonNull
+  @RestResource(exported = false)
+  <S extends Round> S save(@NonNull S entity);
+}
