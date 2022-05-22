07ea7de6aee9023d196aca8e4fffe3370b65d985
BacLuc <lucius.bachmann@gmx.ch>
2022-05-22 17:20:44 +0200

    MatchRepository: don't expose PATCH and POST
    Issue: #185

--- src/main/java/ch/uzh/ifi/hase/soprafs22/screwyourneighborserver/repository/MatchRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/screwyourneighborserver/repository/MatchRepository.java
@@ -4,6 +4,13 @@ import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.entity.Match;
 import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.entity.MatchEmbedProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.lang.NonNull;
 
 @RepositoryRestResource(excerptProjection = MatchEmbedProjection.class)
-public interface MatchRepository extends JpaRepository<Match, Long> {}
+public interface MatchRepository extends JpaRepository<Match, Long> {
+  @Override
+  @NonNull
+  @RestResource(exported = false)
+  <S extends Match> S save(@NonNull S entity);
+}
