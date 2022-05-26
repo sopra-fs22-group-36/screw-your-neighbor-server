f466273ff92361bc3a96b823f8ac3924c1114b43
BacLuc <lucius.bachmann@gmx.ch>
2022-05-26 12:24:38 +0200

    ParticipationValidator: validate conditions to rejoin a game
    Issue: #160
    Issue: #169

--- src/main/java/ch/uzh/ifi/hase/soprafs22/screwyourneighborserver/validation/ParticipationValidator.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/screwyourneighborserver/validation/ParticipationValidator.java
@@ -1,8 +1,11 @@
 package ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.validation;
 
+import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.entity.Game;
 import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.entity.GameState;
 import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.entity.Participation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
+import org.springframework.data.rest.core.annotation.HandleBeforeSave;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
@@ -11,24 +14,46 @@ import org.springframework.web.client.HttpClientErrorException;
 @Component
 @RepositoryEventHandler
 public class ParticipationValidator {
-
   protected static final int MAX_NUMBER_OF_PLAYERS = 5;
+
+  private final OldStateFetcher oldStateFetcher;
+
+  @Autowired
+  public ParticipationValidator(OldStateFetcher oldStateFetcher) {
+    this.oldStateFetcher = oldStateFetcher;
+  }
 
   @SuppressWarnings("unused")
   @HandleBeforeCreate
   public void onBeforeCreateParticipation(Participation participation) {
-    GameState participationGameState = participation.getGame().getGameState();
+    Game game = participation.getGame();
+    GameState participationGameState = game.getGameState();
 
     if (participationGameState != GameState.FINDING_PLAYERS) {
       throw new HttpClientErrorException(
           HttpStatus.UNPROCESSABLE_ENTITY,
           "Player can't join a game in game state %s".formatted(participationGameState));
     }
-
-    if (participation.getGame().getParticipations().size() >= MAX_NUMBER_OF_PLAYERS) {
+    if (!game.getParticipations().contains(participation)) {
+      game.getParticipations().add(participation);
+    }
+    if (game.getParticipations().stream().filter(Participation::isActive).count()
+        > MAX_NUMBER_OF_PLAYERS) {
       throw new HttpClientErrorException(
           HttpStatus.UNPROCESSABLE_ENTITY,
           "Not more then %s players per game are allowed".formatted(MAX_NUMBER_OF_PLAYERS));
+    }
+  }
+
+  @HandleBeforeSave
+  public void onBeforeSaveParticipation(Participation participation) {
+    Participation previousStateOf =
+        oldStateFetcher.getPreviousStateOf(Participation.class, participation.getId());
+    if (previousStateOf.isActive() == participation.isActive()) {
+      return;
+    }
+    if (participation.isActive()) {
+      onBeforeCreateParticipation(participation);
     }
   }
 }
--- src/test/java/ch/uzh/ifi/hase/soprafs22/screwyourneighborserver/api/ParticipationApiTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs22/screwyourneighborserver/api/ParticipationApiTest.java
@@ -6,7 +6,6 @@ import static ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.util.SessionUtil
 import static org.hamcrest.Matchers.*;
 
 import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.entity.Game;
-import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.entity.Participation;
 import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.entity.Player;
 import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.repository.GameRepository;
 import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.repository.ParticipationRepository;
@@ -16,12 +15,14 @@ import ch.uzh.ifi.hase.soprafs22.screwyourneighborserver.util.GameBuilder;
 import java.time.Duration;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.IntStream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.BodyInserters;
@@ -44,7 +45,7 @@ class ParticipationApiTest {
   private static final Player PLAYER_1 = new Player();
   private static final String PLAYER_NAME_2 = "player2";
 
-  private static final Game GAME_1 = new Game();
+  private static Game GAME_1;
 
   @BeforeEach
   void setup() {
@@ -55,6 +56,7 @@ class ParticipationApiTest {
             .build();
 
     PLAYER_1.setName(PLAYER_NAME_1);
+    GAME_1 = new Game();
   }
 
   @Test
@@ -78,10 +80,6 @@ class ParticipationApiTest {
     GAME_1.setName("game_1");
     gameRepository.saveAll(List.of(GAME_1));
 
-    Participation participation = new Participation();
-    participation.setGame(GAME_1);
-    participation.setPlayer(player);
-
     webTestClient
         .post()
         .uri("/participations")
@@ -99,6 +97,54 @@ class ParticipationApiTest {
         .expectBody()
         .jsonPath("active")
         .isEqualTo(true);
+  }
+
+  @Test
+  void join_existing_full_game_fails() {
+    HttpHeaders responseHeaders =
+        webTestClient
+            .post()
+            .uri("/players")
+            .body(BodyInserters.fromValue(PLAYER_1))
+            .exchange()
+            .expectStatus()
+            .isCreated()
+            .expectBody()
+            .returnResult()
+            .getResponseHeaders();
+
+    String sessionId = getSessionIdOf(responseHeaders);
+
+    Player player = playerRepository.findAll().iterator().next();
+
+    GameBuilder gameBuilder =
+        GameBuilder.builder("game", gameRepository, participationRepository, playerRepository);
+    IntStream.range(0, 5).forEach(i -> gameBuilder.withParticipation("player" + i));
+    GAME_1 = gameBuilder.build();
+    GAME_1 = gameRepository.saveAll(List.of(GAME_1)).get(0);
+
+    webTestClient
+        .get()
+        .uri("/participations")
+        .header(HttpHeaders.COOKIE, "JSESSIONID=%s".formatted(sessionId))
+        .exchange()
+        .expectStatus()
+        .isOk();
+
+    webTestClient
+        .post()
+        .uri("/participations")
+        .header(HttpHeaders.COOKIE, "JSESSIONID=%s".formatted(sessionId))
+        .body(
+            BodyInserters.fromValue(
+                Map.of(
+                    "game",
+                    "/games/%s".formatted(GAME_1.getId()),
+                    "player",
+                    "/players/%s".formatted(player.getId()))))
+        .exchange()
+        .expectStatus()
+        .isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY);
   }
 
   @Test
